c1000a7a26c23ef67090e12fc011b3e44358df5b
Kevin Ra <kevin.khra@gmail.com>
2019-11-03 16:55:21 -0500

    CHIP patch updates emissive material. Fixed some missing materials on scenes

--- Assets/Editor/Patch.cs
+++ Assets/Editor/Patch.cs
@@ -12,6 +12,10 @@ public class Patch : Editor
     {
         string path_resources = CreateOutDir("Assets", new List<string> () {"Resources"});
         const string CHIP_PATH_JSON = "Assets/Editor/CHIP_patch.json";
+        
+        // Change the emissive material intensity
+        Material lamp_mat = (Material)AssetDatabase.LoadAssetAtPath( "Assets/Complete_Home_Interior_Pack/CHIP_Materials/MAT_Furniture/MAT_FUR_White_lamp.mat", typeof(Material) );
+        lamp_mat.SetColor( "_EmissionColor", new Color(4.5f, 4.5f, 4.5f, 0.0f) );
 
         PrefabDirs pd = PrefabDirs.CreateFromJson(CHIP_PATH_JSON);
         pd.Process(path_resources);
